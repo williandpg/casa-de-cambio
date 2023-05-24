3d5f07292502d25428d2ff01ec92d647f652e058
Willian Gonçalves <willian.p.g@outlook.com>
2023-05-23 21:57:42 -0300

    Ajustado linter

--- src/main.js
+++ src/main.js
@@ -4,59 +4,45 @@ import { renderCoins } from './components';
 import { fetchExchange } from './services/exchange';
 
 import './style.css';
-import './reset.style.css';
 
 const buttonElement = document.querySelector('header form button');
 
 buttonElement.addEventListener('click', () => {
-  const inputElement = document.querySelector('header form input');
-  const inputValue = inputElement.value;
+    const inputElement = document.querySelector('header form input');
+    const inputValue = inputElement.value;
 
-  if(!inputValue) {
-    Swal.fire({
-      title: 'Erro!',
-      text: 'Você precisa digitar uma moeda!',
-      icon: 'error',
-      confirmButtonText: 'Ok'
-    });
+    if(!inputValue) {
+        Swal.fire({
+            title: 'Erro!',
+            text: 'Você precisa digitar uma moeda!',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+        });
 
-    return;
-  }
+        return;
+    }
 
-  fetchExchange(inputValue)
-    .then(exchange => {
-      const base = exchange.base; // 'USD'
-
-      // Converter { USD: 80 }
-      // Para [{ name: 'USD', value: 80 }]
-
-      // - ['BRL', 1]
-      // - { name: 'BRL', value: `80` }
-      const rates = exchange.rates;
-
-      const ratesArray = Object.entries(rates);
-
-      const ratesArrayToObject =  ratesArray.map(rateCoin => {
-        // - ['BRL', 1] -> rateCoin
-        const name = rateCoin[0];
-        const value = rateCoin[1];
-
-        // const [name, value] = rateCoin;
-
-        return {
-          name: name,
-          value: value
-        }
-      })
-
-      renderCoins(ratesArrayToObject, base);
-    })
-    .catch(error => {
-      Swal.fire({
-        title: 'Erro!',
-        text: error.message,
-        icon: 'error',
-        confirmButtonText: 'Ok'
-      });
-    })
-})
+    fetchExchange(inputValue)
+        .then(exchange => {
+            const base = exchange.base;
+            const rates = exchange.rates;
+            const ratesArray = Object.entries(rates);
+            const ratesArrayToObject =  ratesArray.map(rateCoin => {
+                const name = rateCoin[0];
+                const value = rateCoin[1];
+                return {
+                    name: name,
+                    value: value
+                };
+            });
+            renderCoins(ratesArrayToObject, base);
+        })
+        .catch(error => {
+            Swal.fire({
+                title: 'Erro!',
+                text: error.message,
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            });
+        });
+});
